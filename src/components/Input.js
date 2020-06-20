34045d27b63238b6b3b06bb457de5ebe66e7ab57
skodasha <skodasha@users.noreply.github.com>
2020-06-20 12:45:56 +0300

    add handler on Enter

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,14 +4,25 @@ import {Redirect} from "react-router-dom";
 
 function Input({ style }) {
     const [search, setSearch] = useState('');
+    const [key, setKey] = useState(false);
 
     const onChangeSearch = e => {
         setSearch(e.target.value);
     }
 
+    function handleKeyPress(e){
+        if (e.key === 'Enter'){ 
+            setKey(true)
+        }
+    }
+
+    if(key){
+        return <Redirect to={`/search?search=${search}`}/>
+    }
+
     return (
         <div className={style}>
-            <input placeholder='Search for free photos and videos' onChange={onChangeSearch}/>
+            <input placeholder='Search for free photos and videos' onChange={onChangeSearch} onKeyPress={handleKeyPress}/>
             <a href={`/search?search=${search}`} ><button/></a>
         </div>
     )
